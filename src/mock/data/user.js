e0428e402f925d836b2591203bdcd05d9210e93d
Thranduils <njfu.sjr@qq.com>
2019-10-13 23:33:17 +0800

    travel

--- src/mock/data/user.js
+++ src/mock/data/user.js
@@ -4,7 +4,7 @@ const LoginUsers = [
     id: 1,
     username: 'admin',
     password: '123456',
-    avatar: 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png',
+    avatar: 'https://raw.githubusercontent.com/Thranduils/markdown-images/master/vueadmin/user.png',
     name: '用户名'
   }
 ];
